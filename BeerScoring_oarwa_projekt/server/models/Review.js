3d405a604d9003127d8586fc318781bb690f68ec
Mario Radoš <mrados@pmfst.hr>
2025-07-09 19:01:10 +0200

    Dodani modeli User i Review

--- BeerScoring_oarwa_projekt/server/models/Review.js
+++ BeerScoring_oarwa_projekt/server/models/Review.js
@@ -4,7 +4,6 @@ const reviewSchema = new mongoose.Schema({
   beerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Beer', required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   rating: { type: Number, required: true },
-  title: { type: String, required: true },
   comment: { type: String, required: true }
 });
 
--- BeerScoring_oarwa_projekt/server/models/User.js
+++ BeerScoring_oarwa_projekt/server/models/User.js
@@ -4,13 +4,19 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  role:{
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  }
 });
 
 // Hash password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
